be59356e00ba4351131d02e4065558c6cbfe274e
OmKashyapODCode <kashyap.om8585@gmail.com>
2025-07-18 00:12:08 +0530

    connect frontend

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ const __dirname = path.dirname(__filename);
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://127.0.0.1:5500/server/public/index.html'
+}));
+
 app.use(express.json());
 
 // Serve static files from /clips (if used)
